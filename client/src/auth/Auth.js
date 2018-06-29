d9b5468e96810784f61065a592898674d55f1f07
Eric M Scott <thunderducky@Erics-MacBook-Pro.local>
2018-06-29 13:06:57 -0500

    Revert "fixed?"
    This reverts commit b6c2c1b6d9ea9e8c5e24b295c00b8a64c3d00a02.

--- client/src/auth/Auth.js
+++ client/src/auth/Auth.js
@@ -9,16 +9,35 @@ export default class Auth {
     redirectUri: origin + '/callback',
     audience: 'https://coding-class.auth0.com/userinfo',
     responseType: 'token id_token',
-    scope: 'openid'
+    scope: 'openid profile'
   });
+  userProfile;
 
   constructor() {
    this.login = this.login.bind(this);
    this.logout = this.logout.bind(this);
    this.handleAuthentication = this.handleAuthentication.bind(this);
    this.isAuthenticated = this.isAuthenticated.bind(this);
+   this.getAccessToken = this.getAccessToken.bind(this);
+   this.getProfile = this.getProfile.bind(this);
+ }
+ getAccessToken() {
+   const accessToken = localStorage.getItem('access_token');
+   if (!accessToken) {
+     throw new Error('No Access Token found');
+   }
+   return accessToken;
  }
 
+ getProfile(cb) {
+    let accessToken = this.getAccessToken();
+    this.auth0.client.userInfo(accessToken, (err, profile) => {
+      if (profile) {
+        this.userProfile = profile;
+      }
+      cb(err, profile);
+    });
+  }
  handleAuthentication() {
    this.auth0.parseHash((err, authResult) => {
      if (authResult && authResult.accessToken && authResult.idToken) {
